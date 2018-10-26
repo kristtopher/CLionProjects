a7aecf3167fdd8500ff1767ad55555c0e1346c47
kristtopher <kristtopherkayo@hotmail.com>
2018-10-26 16:40:19 -0300

    include ils

---
+++ ILS/cmake-build-debug/CMakeFiles/ILS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ILS.dir/main.cpp.o"
+  "ILS.pdb"
+  "ILS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ILS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
